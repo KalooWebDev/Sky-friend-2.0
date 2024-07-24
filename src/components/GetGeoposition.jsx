e32e3bcac552419f84b57e179061510293229256
AxoloteDev <axolote.dev@gmail.com>
2024-07-23 22:16:14 -0300

    fix(GetGeoposition.jsx): corrected function parameter and adjusted error message

--- src/components/GetGeoposition.jsx
+++ src/components/GetGeoposition.jsx
@@ -2,33 +2,33 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react"
 
-function GetGeoposition({ onLoactionReceived }){
+function GetGeoposition({ onLocationReceived }){
     const [error, setError] = useState(null);
 
     useEffect(() => {
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition((position) => {
                 const { latitude, longitude } = position.coords;
-                onLoactionReceived({latitude, longitude});
+                onLocationReceived({latitude, longitude});
             },
-            (Error) => {
-                setError(`Permission Danied Error: ${Error}`);
+            (err) => {
+                setError(`Permission Danied Error: ${err.message}`);
             }
         );
         } else {
             return(
-                <h1>Seu navegador não suporta a Geolocalização</h1>
-            )
+                setError("Seu navegador não suporta a Geolocalização")
+            );
         }
     }, []);
 
     if(error){
         return (
             <h1>{error}</h1>
-        )
+        );
     }
 
     return null;
 }
 
-export default GetGeoposition
+export default GetGeoposition;
