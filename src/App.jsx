97306a17d1f52357f3e9421883da5c8e7417e1f1
AxoloteDev <axolote.dev@gmail.com>
2024-07-23 22:24:01 -0300

    feat(App.jsx, GetCityId.jsx): integrate GetCityId component and handleCityReceived function
    - Added error handling in GetCityId.jsx using useState to store error messages.
    
    - Modified GetCityId.jsx to use async/await for fetching city ID based on latitude and longitude.
    
    - Implemented error handling in GetCityId.jsx to display an error message when the API request fails.
    
    - Added the GetCityId component to App.jsx and passed latitude and longitude from GetGeoposition component.
    
    - Implemented handleCityReceived function in App.jsx to set the cityId state and log the city ID.
    
    - Ensured that GetCityId is only rendered when location data is available.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,36 @@
-import { useState } from "react"
+import { useState } from "react";
 
-import GetGeoposition from "./components/GetGeoposition"
-import "./styles/App.css"
+import GetGeoposition from "./components/GetGeoposition";
+import GetCityId from "./components/GetCityId";
+
+import "./styles/App.css";
 
 function App() {
   const [location, setLocation] = useState(null);
+  const [cityId, setCityId] = useState(null);
 
-  function handleLocationReceived(location){
+  function handleLocationReceived(location) {
     setLocation(location);
+    console.log(location);
+  }
+
+  function handleCityReceived(cityId) {
+    setCityId(cityId);
+    console.log(cityId);
   }
 
   return (
-  <>
-    <GetGeoposition onLoactionReceived={handleLocationReceived}/>
-  </>
-  )
+    <>
+      <GetGeoposition onLocationReceived={handleLocationReceived} />
+      {location && (
+        <GetCityId
+          latitude={location.latitude}
+          longitude={location.longitude}
+          onCityReceived={handleCityReceived}
+        />
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/GetCityId.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
+function GetCityId({ latitude, longitude, onCityReceived }) {
+  const [error, setError] = useState(null);
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const URL = `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${API_KEY}&q=${latitude},${longitude}&language=pt-br`;
+
+  useEffect(() => {
+    async function getId() {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          throw new Error("Failed to comunicate to API");
+        }
+        const data = await response.json();
+        const cityId = data.Key;
+        onCityReceived(cityId);
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+    getId();
+  }, []);
+
+  if(error){
+    return(
+        <h1>Error: {error}</h1>
+    )
+  }
+}
+
+export default GetCityId;
